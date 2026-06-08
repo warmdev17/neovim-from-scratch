af6019af62345ec65bee73331c57d45af2d20fc6
Hieu Le Trung <warmdevofficial@gmail.com>
2026-06-09 01:21:31 +0700

    feat(options): add more qol option

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,11 +2,11 @@ local opt = vim.opt
 
 opt.number = true
 opt.relativenumber = true
-opt.statuscolumn = " %=%{v:relnum?v:relnum:v:lnum} %s"
-opt.fillchars:append({ eob = " " })
-opt.cursorline = true
+opt.statuscolumn = "%=%{v:relnum?v:relnum:v:lnum} %s"
 opt.signcolumn = "yes"
 
+opt.fillchars:append({ eob = " " })
+opt.cursorline = true
 opt.showmode = false
 
 opt.clipboard = "unnamedplus"
@@ -24,6 +24,8 @@ opt.autoindent = true
 
 opt.ignorecase = true
 opt.smartcase = true
+opt.hlsearch = true
+opt.incsearch = true
 
 opt.termguicolors = true
 
@@ -35,10 +37,10 @@ opt.undoreload = 10000
 opt.mouse = "a"
 opt.splitright = true
 opt.splitbelow = true
+opt.equalalways = false
+
 opt.timeoutlen = 300
 opt.updatetime = 200
-opt.hlsearch = true
-opt.incsearch = true
 
 opt.swapfile = false
 opt.backup = false
@@ -48,6 +50,7 @@ opt.autoread = true
 opt.cmdheight = 1
 opt.laststatus = 3
 opt.pumheight = 10
+opt.completeopt = { "menu", "menuone", "noselect" }
 
 opt.foldmethod = "expr"
 opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
@@ -59,3 +62,13 @@ opt.hidden = true
 opt.confirm = true
 opt.shortmess:append("c")
 opt.whichwrap:append("<>[]hl")
+
+opt.grepprg = "rg --vimgrep"
+opt.grepformat = "%f:%l:%c:%m"
+
+opt.diffopt:append({
+	"algorithm:histogram",
+	"indent-heuristic",
+})
+
+opt.virtualedit = "block"
