fdd127ec5cc8f1b07e41a40b2fbcf4e665c9b79b
Hieu Le Trung <warmdevofficial@gmail.com>
2026-06-09 01:10:34 +0700

    feat(options): add basic editor settings

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,3 +2,59 @@ local opt = vim.opt
 
 opt.number = true
 opt.relativenumber = true
+opt.statuscolumn = " %=%{v:relnum?v:relnum:v:lnum} %s"
+opt.fillchars:append({ eob = " " })
+opt.cursorline = true
+opt.signcolumn = "yes"
+
+opt.showmode = false
+
+opt.clipboard = "unnamedplus"
+
+opt.wrap = false
+opt.scrolloff = 10
+opt.sidescrolloff = 10
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.expandtab = true
+opt.smartindent = true
+opt.autoindent = true
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.termguicolors = true
+
+opt.undofile = true
+opt.undodir = vim.fn.stdpath("state") .. "/undo"
+opt.undolevels = 10000
+opt.undoreload = 10000
+
+opt.mouse = "a"
+opt.splitright = true
+opt.splitbelow = true
+opt.timeoutlen = 300
+opt.updatetime = 200
+opt.hlsearch = true
+opt.incsearch = true
+
+opt.swapfile = false
+opt.backup = false
+opt.writebackup = false
+opt.autoread = true
+
+opt.cmdheight = 1
+opt.laststatus = 3
+opt.pumheight = 10
+
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldlevel = 99
+
+opt.hidden = true
+opt.confirm = true
+opt.lazyredraw = true
+opt.shortmess:append("c")
+opt.whichwrap:append("<>[]hl")
