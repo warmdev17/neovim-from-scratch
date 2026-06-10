75289deb885f0c3bc499a939e060c75b968aa3b1
Hieu Le Trung <warmdevofficial@gmail.com>
2026-06-10 15:50:06 +0700

    style(ui): position which-key menu at bottom right

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -190,3 +190,34 @@ wk.add({
 	{ "<leader>d", group = "debug/database" },
 	{ "<leader>t", group = "terminal/test" },
 })
+
+wk.setup({
+	preset = "modern",
+	delay = 300,
+
+	win = {
+		border = "single",
+		padding = { 1, 2 },
+		title = true,
+		title_pos = "center",
+
+		row = -1,
+		col = math.huge,
+
+		width = { min = 35, max = 50 },
+		height = { min = 4, max = 14 },
+
+		no_overlap = false,
+	},
+
+	layout = {
+		width = { min = 20, max = 30 },
+		spacing = 3,
+	},
+
+	icons = {
+		mappings = false,
+		group = "+",
+		separator = "➜",
+	},
+})
