a21414a6bf5d12d2a12526a260bf398755e241e2
Hieu Le Trung <warmdevofficial@gmail.com>
2026-06-12 11:16:08 +0700

    feat(editor): add more option for better oil

--- lua/warmdev/plugins/config/editor/oil.lua
+++ lua/warmdev/plugins/config/editor/oil.lua
@@ -15,11 +15,20 @@ end
 require("oil").setup({
 	default_file_explorer = true,
 	watch_for_changes = true,
+	skip_confirm_for_simple_edits = true,
+	delete_to_trash = true,
 	columns = {
 		"icon",
 	},
 	view_options = {
 		show_hidden = true,
+		natural_order = true,
+		is_always_hidden = function(name, _)
+			return name == ".." or name == ".git"
+		end,
+	},
+	win_options = {
+		wrap = true,
 	},
 	keymaps = {
 		["q"] = function()
