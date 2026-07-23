7d009f6aea2b01e3e940a9073487863ab7329667
Hieu Le Trung <warmdevofficial@gmail.com>
2026-07-23 15:10:59 +0700

    fix(completion): enable sql completion in sql file

--- lua/warmdev/plugins/config/completion.lua
+++ lua/warmdev/plugins/config/completion.lua
@@ -22,12 +22,16 @@ cmp.setup({
 	},
 
 	sources = {
-		default = { "lsp", "path", "snippets", "buffer" },
+		default = { "lsp", "path", "snippets", "buffer", "dadbod" },
 		providers = {
 			lazydev = {
 				name = "LazyDev",
 				module = "lazydev.integrations.blink",
 				score_offset = 100,
+			},
+			dadbod = {
+				name = "Dadbod",
+				module = "vim_dadbod_completion.blink",
 			},
 		},
 	},
@@ -119,12 +123,6 @@ cmp.setup({
 
 require("ts_context_commentstring").setup({
 	enable_autocmd = false,
-})
-
-require("Comment").setup({
-	pre_hook = function(ctx)
-		return require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()(ctx)
-	end,
 })
 
 require("nvim-ts-autotag").setup({
