9e32e09754f18357cda46d3f53ed10bd438d1f97
Hieu Le Trung <warmdevofficial@gmail.com>
2026-07-23 15:14:24 +0700

    style(markdown): make html tag more visible in markdown

--- lua/warmdev/plugins/config/editor/markview.lua
+++ lua/warmdev/plugins/config/editor/markview.lua
@@ -1,7 +1,17 @@
 require("markview").setup({
 	preview = {
 		enable = true,
+		icon_provider = "mini",
 		filetypes = { "markdown", "md", "rmd", "quarto" },
 		ignore_buftypes = {},
+	},
+	html = {
+		enable = true,
+		headings = {
+			enable = true,
+		},
+		void_elements = {
+			enable = true,
+		},
 	},
 })
